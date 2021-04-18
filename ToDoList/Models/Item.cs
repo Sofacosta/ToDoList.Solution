3adea73dd405e12714c87ded6149a3966775ade7
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-04-18 12:58:29 -0500

    updated to lesson 16

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,20 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     // properties, methods, etc. will go here.
-    public string Description {get;}
+    public string Description {get; set;}
+    private static ToDoList<Item> _instances = new List<Item> {};
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
+    }
+    public static List<Item> GetAll()
+    {
+      return _instances;
     }
   }
 }
