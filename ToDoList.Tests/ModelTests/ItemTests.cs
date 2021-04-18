6c504d28b6cd6f2fe237ece5bce2dbd52a50aaf0
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-04-17 22:44:00 -0500

    passed first 2 tests:cerating an item and item with string list

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -8,6 +8,24 @@ namespace ToDoList.Tests
   {
 
     // Test methods will go here.
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange 
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      //Act
+      string result = newItem.Description;
+      //Assert
+      Assert.AreEqual(description, result);
+    }
 
   }
 }
