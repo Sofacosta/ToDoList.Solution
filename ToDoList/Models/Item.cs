abf6346021a8a9a8f1e9c7ec6e22c45b42483369
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-04-17 22:44:30 -0500

    passed first 2 tests:cerating an item and item with string list

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,8 +2,11 @@ namespace ToDoList.Models
 {
   public class Item
   {
-
     // properties, methods, etc. will go here.
-
+    public string Description {get;}
+    public Item(string description)
+    {
+      Description = description;
+    }
   }
 }
